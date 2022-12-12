2921382fa8cba56b9ef4e4666da951df7cb11cd9
CMAK12 <dsumcow@gmail.com>
2022-12-12 16:52:40 +0200

    New Update

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def main(request):
     return render(request, 'main.html')
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    reversed_text = user_text[::-1]
+    return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text})
